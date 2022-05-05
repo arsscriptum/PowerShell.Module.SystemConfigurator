e022248045ea8ca5ee348a050a561716291e9c68
The Art of Coding <96393853+arsscriptum@users.noreply.github.com>
2022-05-04 23:29:46 -0400

    auto

---
+++ doc/Remove-PnpGhostDevices.md
@@ -0,0 +1,103 @@
+---
+external help file: PowerShell.Module.SystemConfigurator-help.xml
+Module Name: PowerShell.Module.SystemConfigurator
+online version:
+schema: 2.0.0
+---
+
+# Remove-PnpGhostDevices
+
+## SYNOPSIS
+{{ Fill in the Synopsis }}
+
+## SYNTAX
+
+```
+Remove-PnpGhostDevices [[-FilterByClass] <Array>] [[-FilterByFriendlyName] <Array>] [-listDevicesOnly]
+ [-listGhostDevicesOnly]
+```
+
+## DESCRIPTION
+{{ Fill in the Description }}
+
+## EXAMPLES
+
+### Example 1
+```powershell
+PS C:\> {{ Add example code here }}
+```
+
+{{ Add example description here }}
+
+## PARAMETERS
+
+### -FilterByClass
+{{ Fill FilterByClass Description }}
+
+```yaml
+Type: Array
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: 0
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -FilterByFriendlyName
+{{ Fill FilterByFriendlyName Description }}
+
+```yaml
+Type: Array
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: 1
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -listDevicesOnly
+{{ Fill listDevicesOnly Description }}
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -listGhostDevicesOnly
+{{ Fill listGhostDevicesOnly Description }}
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+## INPUTS
+
+### None
+
+## OUTPUTS
+
+### System.Object
+## NOTES
+
+## RELATED LINKS
---
+++ out/PowerShell.Module.SystemConfigurator.psm1
@@ -0,0 +1,125 @@
+<#
+  ╓──────────────────────────────────────────────────────────────────────────────────────
+  ║   PowerShell PowerShell.Module.SystemConfigurator Module
+  ║   Version 2.1.16 , Generated on 11:27 PM 
+  ║   Description: 
+  ║   Current Git Revision 52f80bc6ce0c8e508c2b383e0fb450b0ac087357
+  ╙──────────────────────────────────────────────────────────────────────────────────────
+ #>
+
+
+# ------------------------------------
+# Script file - DoAutomaticConfig - 
+# ------------------------------------
+$ScriptBlockDoAutomaticConfig = "H4sIAAAAAAAACs2UwWrCQBCG74LvsOSeJ+ipNQVFbMXQSik9bOPELG5mZDKbYEvfvWsTRSvqJQGTS9j52PnyZ7L9Xv/vTh0mYgjVCEtaQXjvhHItJhkQpmb5vWWUv+ZsBMIhFaJCVGGqIs2rGSxU8B5vihr+UIG6OyJr7A2spUoFMUg4989jpAqnWrIiqDc/LbTZ9gmqcEoVcJz5lcgwJEK8iYVdIo6hkbiGtZ3EDJamEN4881Kj+fKZEw7Hg5eDTM4hraroEg7ee0ILZ+HBGbsA3qlcQDpUiaAES+scUOqW+3G5xrX9qZptH7E0TJhXKK+ajf48ULpMNVBGVUPtKyNM6QYynDKlxrY6482BMoOUoch2qcBpdtfBDrTqNOLM5CM0cmxyXOu0+QTQ+dlx63MGe6AzjcifuZa0/5fPivxDbnZKfrZmvV+hKsyu2wYAAA=="
+
+# ------------------------------------
+# Script file - EnvironmentVariables - 
+# ------------------------------------
+$ScriptBlockEnvironmentVariables = "H4sIAAAAAAAACu1X3YobNxS+D+QdxNRgu90xpZcJDrjeXXdp1jb2dkNxF6ydOWOLaiQjadY1qaGFQH+gaXuXXvRN2nfqI/RofjW2N0lJycWyXuSdkY6+c6TznU/ywwcP078oEYFhUpDpUq79cxkmHC6pYvSagz4TkXxurQh+nilmwP9MakM8+177/PPnH79he4ntO2zf5+8/kLzjJ2y/Y3uB7RdsP+d9Pzrjv5LM6NVfudGL7PnV3xYpN3xZ9+sR/1QqWCiZiJAcU/V1f0NFFnADV3MMN6RLBmDypWnsGFOzrCxsJ5osAkZ8O1JO84+ZgsBItUEfjBtQpDmWa1DTJXDeyaZ2PmxmUBjFCQ2WrUZMmCiB27h7RagNQWNAT424M8QnZ8D6T0c7esWZaTU7zbYzfCJuMCN2Dto1EblJPsJJs0+uHKNolUGfJpxbqGqoenIz6KLOTbf7ZG5wL4dyCOunTIC7rX3JpSLndAHC0MNo8waIm0cuprePYLMzgbDg0zb7V228dzK8fPSVR74lz5aggFxSnmAgMTXB8rat3+7zuMo1hsOUFPFamJLSJM/IrB+HHMynTIRMLFrTZLWSymisgoSHYyUD0Lqdb/CYKhpnj61q/bO0G5AXrXMqQpoyBXMQUa6hmJoa6jXDJVw1LkCbrLtt03LP0oMsrZ5Y1Er3zK2iHeb5gvhRwSzizaz5FfFut8+Mv0QayTXxLFkKlkDFEiyEtC7cKpkjozNGYne0sq/TQK7s6xca1NxznG4BKfD2QTte3iL2gQIQt4e+E/QO3OsX7E4tl2tXWy62adfa/J8xp1hrWLoO7HaXDtu81A+U+UAaeZp3v4fyNiqBI7KSmqVxdMnH9Vo3Ch1eNayiHb2LWEwS8U7zUX0D2FMbW1LpSO1kGuKhgWgTiOUN+GcG4uIsTL+tkgeYzwkEidL4cKKUVL0sEWcLgTpfk3R0Yuswg6gB2e+LDeb8KdDIDcEsla1HPGcht6VcAQ03BL5h2mjSyoOI6eYa2jmrU0oUFOxLYZB+dYc53T4gxUk/GF2MSEmXuZgLxLpXYkeJDwqVvVVZpRKFRjnsKXazL0N7Q/HKGh0neum7l4LnNfS5Zw1C0n1C9u4PKK+Ps/lcBjRF27N5TLaOujV6nFFdxIDXGD/tqF2fSgHfjcyq+THoQLFV5st7g+9K/gdcXlNe1ohbGlMjV8QfZZATZPNI8M1Rj/N0Zm0Te2H4OgLX9ni31qxUuLV0MIHFieek8M0nEwILVDMiYE2isjSdqR1U8p2ug2j5mUVmo89L14Wk3yrqaf7ex43tXtLvJf1O/wT8z5J+p7W0Vyzujgnpg38BokIG5dQRAAA="
+
+# ------------------------------------
+# Script file - General - 
+# ------------------------------------
+$ScriptBlockGeneral = "H4sIAAAAAAAACu0ca3PaSPJ7qvIfplTUAXcW2f1K4lRskG02NnBIti8VUo4CA+gsNFo9YrO5/PfrGY2kkTQCgYmdrZitzbLS9GO6e7qnH+Tli5cvZqEzCSzioD6+U4fkDnv6Att21/LwJCDeSg+8cBKEHkbfXsJyBJ+PneXUxsGx5UwtZ97QQ9clXuDrCxLa06FHJtj3m5+ita7pmcvoa6MZI7j2rACrhhXYGCkdD5sBXkdZieFq+sSz3KA9HFxrI/1MOz+/GR4ZZ/QV+xyiP4jlqEMzWCAlXnzxoTvoXF5ofUNnqxWkpMSUHGL95mLQvTzX9AqIc1wA3gsyDW3sS5F2tasszu2Qql38FdvEXWInKOIfjgYnvXNtJ6ZBYTPLxlKkf2gdLrXtkf4XtEhFEeG9Yyr3l/4cdvQBJSppR2hLlVWETpWHzsgSUwRlfEhIRxriVNEa3ZcDI1BFjECuYxnXXCAC5TJRy6GpjkrYFrWfnhUD3wegLsVYYB8jEw5wsMBoEnoemBCcy2Dht9+cWw4+hgN4i169jeDeHBN7+lZUUSv6tEuVhN68YkBIgi6nrSyi/DmWwHOZcx5yjOSPazk81VkZfHIyZexztZWRz54ROQKquVL+MydXpo7iI1DoCt1ZINEvoE/mPafImiEHnDi+t/zAdAIg8/IFs4UIBnzznXoBftmc42Nyj96l34fUQ/tI7RAnoJbBDIfbEHC/dKkVneJAvTI9y/wCLlvtm0uMhtf6WYplEAZuGCBYZId44NgrFKGwZo0Yi+pgVP+A/XqThpLYjj0MLt6J/v97+jj+RE9obOoFeIk2OR62yli5GCVhBKknxJsA15rnEe8oCna9uUPgRCD1emEGvVk7RmhoukFtQvsfgh2p/RCk/JrHq4vBbRytpqU2rMDyDTzn/MxT8JxnoRrPiXt7MpZTDipwnHWqT8NzjodqXKfO/MmYFligPNN/4HBm7os6uIQRnoO/8VYDb2461l8mfXH2vnP5gLsiaqRO4CPzTTjAXmNIfItiP/zt4MJ0piaVxGENbog4hmYAwAyQ+VTrTcGVWTMLe9HLJhN89v55RnzqlJA6Q13Tux2BF1U+wq7KNvUJKUXwCPYDxDdyhxSU0kUgUuH/DpLIq/Wv2gV5WX6igPy73MWsAAohnv63PdbJLLiDOD9W0L/ApFLi+atdEpLB5c+seegxZBRRLY99nC5uZVan9wyNhhwfoA3sB5E956y6ZEc0NHBgFf+Jaiem7eNmGhpqzFoPSw9LAe22p6X0sKSBh0ekylstke2edlyG/UdvnPJdWCrdAchIZt6HG42BnV7N+Wp5xKE5zqd2G46i8CC+fTTqeQz1gzLcBwr3MEpzayolsk6JlSx4CM1tdnbpw8F+zG2JBL9jULug9TUONTK1xHeu958lxvO51Ho+K8r+uSiR3WelSL1cQZ+ZwD4rTU6Shc+MG34uYvxdihiRJyym5ZsiavqckVpfpoidPlB1sResqjt/lo4JJ5vC1Xkqti5XYgFDu3fhPqWze1MSM8rjQ3qP3EUgUTaeFYa07PJQYaSEJIIoJGBPJQihpieRSa6atB+R5EiWSUdI9R5fOPG5y8tEViV78KkRaEmFkc8in0Qc1LdJpCFW/fYkjJhSmSzE3PThosjnlJ0FntxSwhZkePgYz+C2qgemF+yl+cCvsqpDgkb+Bh+zSg1/NBgYzWYSgtjnW6wy7HlI6Q8MpP2npxu9/mlbhO2OelcazZmjshaqzegF+fX3asRH2unNpa6NbuAeJDJQhbgIq+Tpoy0YOD0fHB+dZ1moykAKu7MM8hep6HGzWYUFOayUlWrMXAP6nnGjd0a9ocHMPpJJFWaKsDvLpDPon/ROL0dajpMqbEhh14gkfkxLK69lZ7S3pAdO1Vc+eJrMPRnchb/HLmENCjHaPYbbWmMOtaYJWS7B06C6h+ctfI/rzZZOQnA3gjKrZqn/UDhyuOVZbD/wpcSQUyjRacdFIi4OtBF4C+LJMd6edA40n6eJaCIfDWWqrbeyNZY8V9yoMqXEa4hK7x1y51DLf4gZxZ4+02S+hmsvet8fXPcRtX8dNZIW1gEa9joGHBD4phmdpsLrncwEY89+ZBzdMN9Ok4f46TlsGFofv/3e0rENlwg8pcG3GODjt2hESIC6nvUViwW/FHk+fRkNTkdHF7ledEn6ImBhmcucdnVyCAuNu8oIE7gCxlh4FdK2LEYzMMdDizX4i3xe9braYIeNM6xX1hSTPE7wfprW188G2+9d5HQMKzB2/AVhze2MkYB1nQ+Oujty3QXbt4k5LaDV9PfGYJjHWhXtNRwccuePu9i/DYgrbcjzd2l7WUZb1pbmQkkAS8xCAiqIMYIu05MENpZUgWxGA1LASUhDVcLwFmMHkVFJ2vcZY6WAu3fgS9vx/0QXK5Rwv30vHjWEPnzkYCk7X0wfvBI44GBh+U1Zrxlx0yj2rkXTkAHGtiGZHsi6DBmwYB1Z4MIplvLM7aPIc+aEykAjLUta9VmX9CMa9X+/Nv36XmbGd+yvQyEmkOvI513Q/lqplVkouLIn4CHrEp+AgeL43f6aVdV5EF30EzDw0DmVLViguQUlznz9CYQC7NGOZZ33iAcnxvXRSBtfWBOP+NAxTi4InSguXcF6oDyGuo4NpL1xFDQ4pno+oSmUnBQZAwoawp/EMe1CWUliHw+tKzFPCp6wCl88vhXYynmvx2SpDrE+jp7FMlzRrz02byxGyhjLHrJ9sCUY2t7tmrbn0E9k3Fv42mfj/mmNu6IWn8can8caZd2257HGxx1rjN2DZN4j6ZuutgnG+gR8DKrT4FKviD7KOTf7nK1R8ztMfdPlZgeek1pEvUrM+/kICKlZkYAkb9uaQHrjrzKJkUWfLY7TnH6EXXLp2XFCvn1NXDpTW2WQlu4mepz2ZSgnUNaaW8E7+HcRfmlBb6Zter7P9hcuXzEwNobaghW0op7WEBh8oQNgfsXCr8+iWYXj0KKReY8tJTf0FzAOtnbOogZEaOWUl1M6vO1Et0vbTsIoJ1/Y/FY7tYKoUxU/462pdOiL7yUSHScP6GPFFmZ4WhkRcJsr28Smnw9NbOLgtsgEXwStqIh1hS+inajCqohgKXNjH/TqchZrfuBmStNQV7ruNlvsex0OTei2XP93vroF5AAgpkPcqWTOPGsawgBL/NueZ/vI2Ectlsshetcoag3muD1cP5C84BkTHYmUvjfMWzy4c+CuvLBc6YpT5g6kr2AOc2rJ8UKwZrPq0KUteb9yA9rQchcr6QJ9YS2lL/rByJovIIjIXsZlYjheUqQ4OJrY0lcneKrdy1mFEipM0MgxGthbWpAmSl8eBYE5udVDOLXS92fB0qbewiuRISQ21syawK2nzhugkJBo5mTRqC0JlJ2dxC4y49qxAyp3ShRc3vuN3QpfL3SIpX4lXpZaauIeo5P/S7i3nKMrc3N8FGuPvq18PMKgwb+81v9DwiJDCfUVKH/SAY8GLbUYmI4OmN4qqYk2WydwtXfADJuirjig6PREe+iAqmlKLmg0Ox+ctk3ow+oxmQPU11hXijP9hQy5lfxkpBxvKklyK6qlaKdZuIIYBsMPKKmHtVK4G5f/KBUsFanqWxSQDbPJ9NMh7ioqbiubkCrr7rql9RFhp8lJicwEbOBwhJcETkiF6vo4VjaCezN0W6H3ulNpXbp1iU1txcgGat8l81bOV3KLAcMMUtCFJOXYx7BMesb5tSEl15ospxByivSjIaxcrIHFLNZQTJlAU+I+KIBgZBn/IdSAKYaUg6JHyS7NjP5wMOoQBOZRKr3Qp+8E9EqOcNzH5yvWodnYz097+gJB3rMvW559nONut5ZxZhZNsm/uob+jUnvMxEgW+HZOTtPMUhx1hqhALYUdu/hFs5WEhjQxrWXmxcXgnMEn/p0UDIwxLYVJsSnrbwUc1RA8s3pO4PJFBZQiTm4BrVfJ1SAGIW4CkRl4nPKRx8wpgZFHPOWBNh57TGqFXCHxYjVyLcc2mdzChCifNqRoFdDn6/y9I6PO9Er8YI0KXuWHanXNX4+yk36FtGB3HSP136GFA5mq1x4nmsj0HCv4JQVPN19J2MoSElGkWkiNQ8eWIr7ATvjrGrgOEtjWtHsOTCvZNgQaJ9xk4bFXgt/rww85/sIqJRjRju++nTa9G7ERVZgvsOBPqJLSL6eYzf+KK9gU42RBJsSG5HY1Dgix/TEE9OUcO+wOgtSj6dQgEctM+S/+Dz/OrDvaSgAA"
+
+# ------------------------------------
+# Script file - Git - 
+# ------------------------------------
+$ScriptBlockGit = "H4sIAAAAAAAACtVZ/3PaNhT/vXf9HzSPG3CrYbvb9gO9XOuAk7AS4DAk67U56oAAr8byJLkJy+V/35MsGWFsmjbt1nG9xkHP78vnfd7Tk/L0ydMniySa8YBEyJvRIOatbsS4H4b2ZRDNyQ07DTi6ewpyCD5v2ut5iPkxLAXRsuYlcUwoZ96KJOF8SMkMM1a/SmVjn/rr9LGW/pAahuJrzDGtnfvR3OeEbo4qnCb42ZCwQDhy9FP9SogaLzFOwd5VZULDZ5+m6+dSXSrOoc9X6Wo9/aEX8BwdITTGjNuAQfZlKhQsalu5Orq7oQHHNgvJElkKOCSQ80OK/fkGBVq20WhY6DmimCc0god75ZqpQCkGL5Gpa0HJGgkIECfIdN/S6an0yMwPtV8U/P+dBJEtZJBVcfsXrTFexxayQF0D32Irfe0Uc3sQgT18EoS4T9r+bIWRLSxZwp6FUhU59RkSdkR4TQJVJFav323R5ytKbpB14oOhuYhjibkMTgNElU/3OiYTmHx8zf5g5HpjZzRGTW8yHMIv3rl3ejz4w/VQ88Idvfa6PbcvVoeo6fR6E88dwUp/0Hb6bbcnn3oDz3WGw1637Yy7gz4sd7qjoxy+Ww59jxy6ZC204jxmrWbzT8rIgt/4FDcIXTYDtsJh3IS04dtGvIpfcBIHsyMG+Y5nUDwAcqrnh3w0kJZtPOKXfETiu21MUmIo/tdxpQrSyNRzPjbxtYjunWXG986CCCXgRf1grwJEOyhoBjt1X6tnnAQuzd5LYhyh6qz1FtrEEiTYW9BZ1VLBAtUqbbJeQx2DnGCk/q26TMlaRfqr8SbGyInjMJj50lPbpZRQJ3W7u4wIxVCTW9ZdSg5dYHpNGEYWKEQBQyDLgedDZ3z2DPmMJWuMwCW2LVaVeBmGaCsZNcUPHDIs3BbsRyb9s4Drh5xQcKIFCedA5TnBLKpyhG8Dxk27Cx/sHDAsoVoF4bzL8Ro9xgVpmqHrRKIDjYJvPu7IJ6sXKQX1wndtwjAiHRddiAELXtbyfHkLqbOvfbaShHiGCtehzMqWr4NIiBxaLtctVjPbarPY3VtOCHWhd9YqwCsRBDDJDMlshJr0e+xRr+4mrgThzM46YAxq0EiX/qiNxsye/qgs5h53iX5f1BEu/WC/IzxuQFBwqpaxo/socz5dvQSy49qujI3/QpUTIWVi7IUYx8j28IxEc4Z+LWVqW6RIbLeQEbkZMdQ1NmzYsg18cs6VDAjZ/pUzlVd/qPN2ow/kPbaV7Jecx+q73p0RxpF19Fkfa19Rtw97WK+HLrt9dNodW1/Q1L4qMKDRRGJiaYkcKQBhbQpOdAaX3vS3X6aTUa/AFwGoViBK0FQAL0+FEhXRVOwV22kLaLUI6BpIMIIhL1VnA+rQ1lBVqzQnuBeotmlGddj1dPlnOuwIo+qmulv1hps27HIL1PHp+xHQznrz3RUySGkKplKvcRiKSQu5t9DvTNm0I+w08oKRX01/5UAizUs1XR5GLON4KcvBaDskgMJ/c9goOiGMcEw+etyQvalcRx/eyI4X2/bm3mKgTQ2mX3uWn3PqDY8kdJY1PMmToXdMkmieucAabRJxH0aVV3hTqwoz1aLNXnKi+gZScoWqJoXsPunjm142j+bLYiaTITFogGeQ7wVSpDI+23dhnk3jstS78FPPyAaNzibHU2cyPpuKkbVVtDAevHL7L8HkKrluADilbzcN5ywJtD47iMHk7ltxLUPpXj/o/++LC8IDUoC/Ew/Tjs/9b6gg3DWc2z7vAP41a0MfVODoND0fdNyDZQDzg8+7C1UNx2GCjVKAA7lZA+KgIBv0Etn2MiTXfogShmkjAs8RHKJEBHB6EppOqb8proyv4gEWqRAuyJwc9CFfDg9uDDlfHorEoS7xRW0XYrBvfceBnV4glcKDVguPWVDwvA3rE9+XjqUKtGt7LaCk+MVx7lQ2GGcm6nkMe2NUcuL+eAPIBmvoR0t1Bt9aOCfzJMSwAgczutlehFWk0SHFDEdcj7ha7MKXhFW7vlILyLsRLKLqHC/8JORTX3o/5UJTNu/Ucqrl2C7aBdop2VRIufp4u+YxSGremX2kV270oTVyOx0YXJx2G+5cZKeXQ1klSmCKA/8yHSXCWqEWE+/JNJenuJM6PQHOyDr6l9Ms7IpOK0TF7QGojjHlmwKURR1sQU6Uw9XCm5dGXswAWposhlUu7WWl4544k95YzpRiZ+075+6BvBSKi1vWrAlkmh+gzdRQktwHpFeOtRLR/0t+5ZA0jWHpIxneCj4mx2p60rmDU0a/N3A68uxwIDu51+Cys++m7zzfp1HqcAqYmM1gOoFhReJwwEJxGg+xoZQPsGXMEu5fh1LH45kAKhUTdiYlMTHIq/3ixOnpydwVAaTaVp+Jhu2ARuNySl1RKdGdi30NhVpTC/peyfS2jI1nr3rnrbfe4GR86YxcceMG12nqRJox1DhzllBz7886KQmKwrsz/coef0TVnXvCLAjQUwrFF0WiIv5SM6BLPwr+lhfcZ6/aEwHItwLDV6QL/HvyD/cBYviWHAAA"
+
+# ------------------------------------
+# Script file - RemoveGhostDevices - 
+# ------------------------------------
+$ScriptBlockRemoveGhostDevices = "H4sIAAAAAAAACuVb/3PaOBb/vTP9H3RpZpZcgSbp3t1edzpzgE3iOwIMJs32MpmMg0XwrbFZ2yzLbvd/v6cngSVbMpBurp05fgjElj56+rwvenqyX754+WK6jCZZEEdkROfxz7QxjBYXszjNLPpzMKEp+e0lNCLsw7+GXuLNa+ISufWSxFvfHXeDMKNJe90JvTStm+52k4BGfrjue3OaN0pXQTaZ3R2HwXbUQRSu9Q1k2Vgr3uhEEfHVgslIYUgyXWa/BtEjv36c4BQ3M3tPjrNkSfm9YEpq6ixII6LkOFqG4QkjgctCyCoJMtpgUpAjpf07ovY/4n1+V0RThpHpOGA0uZs0qHzZPHaBZNKgP3EajMP2oAfxBWegiVm8zAgy6YUwfAFQjKyne+qFKa0UrajefeV7xN/VUhaxnyaq+nWc0my58BYB9PmHAFymYHHEXacZnX9fvtS0Au8xAlGCSaq7Paa/ZLrro2WUBXPadCJQd7xwaYLCiqYRaD1deBNKboLo7Tm/KDG2WD6EwYSkmQfjkgkauMtkby2CvJXUAT9v3pAz4HUaJ3MhjEfQuC+uHYvEzEuRbGJHyzlNvCxOgAiQcJglzX+DmCrerRWGznwRJ1ntaENc0w/DozrpzLwE5IHe4leztczikzsVQJ0FEEWTSAzHZ2MFFzRDCUGX6TZMbT8JnZKLZeCLWcCvbZjZfgRePiVjk9kq8iEe0igrNwmijHRD7zFV75wIfUmWlHN9HvkbrsEKvUimVSI718BXzbVoW8F0miXMpp6f6TLbB9NTZ5PueWlmJwlaOQalfTh7iONwwxibKo8yTjSNy5xt5pu3gY7lKZMlm/QVnT/QxIl8+oumCTN1d3hv2R+cfndwb7XGLQnW8jJvX8us4OqP4MSiYAfxOpeNRfQ9qalW+RdSMrgFF3JEH2EqyXoI8Zom2do8KX+HvnXK9BVlmoxkIcbW3GdL5DUI8PZ82wokZmDj9SJP0PLPwzqjt3fbxu3ldEqTXQPzVm7wK60WIaE/LYOE+qzlk9WqUd9nxTutWp0IGkXw7T/dgXd7p6aTi/GyvQxCH3LbojCaDkwPxWYVmmDNR7vV8IQ48ZzuxjcufJo1Lf27yZZNbJPvsRkB48sJiLgGemr8619B5DddYAkWo8ALZXG3orJOxRFNWZbohD4zeWC0f6+7j1kL5m3s1/dGCIgKTNfaBoKchKaQPFLfMGdcS8uzorB0kXzNx1bkHQ5qTCAt56LTBRa6reveWFx7T05/OeWfszqmPSy1IZCnwwQxv9n0+uB0bKc/tkfdVsfWAQ9Htmv3NcDndV3zVq/X6bVc13YLzb/VNh+OBl2nZ5fRv9M2L8irdDk7rWu5lqndtXiw1GEX32Bz1oibXce2bLeT35HkP60z1vkosPhOSG305uZEi3TZGlk3rZENuaYO6QyRLr3EX0FyBo3MSJ3B1bA1dto9wEL+dVpjaJ14vgBXfwgBL60AvO5fu7Z1qtyRwN4i2DKCVNrX9nftEWPJ1P9b7C92WTvFODPB/GWXGLz/uan/X3f1R4tWriv9/8Y5xf1ebdRoZAH1SRbne4gTMypuMbSo3+Wo2MhMjzVyPtgjo3h/56aYBD/DglZlPP2uc9HttS5cHUpLGE40DR55XDJDXXUvTMK0EeZq+ljRvTty7L7V+9hvXdkaiA5CKIUdM1Zv0AGPGPTv2RoxusLfBTwL8XrxBDwijthqlczxZwXs8PKj63RaPREG7gftf9qd8T1KLEHbCD2crdNg4oU8Ggwe/kMnmRDboIrWsNV2es7Y4WG0IHCXq8JbeA9BGIC1MV0YPMe5719ftRXrUEImQ7oO+ku2yzGjDIcQcJ0ehF2NPGc8Ql0vIITyQlmVbfQGN1VYPD714tU+WO1rd/xxaCs+JGHx8NRepizVFj5kEMq+aHU+Cjg9VTxW9eijN1kLSCMcAFWxzuMVgOyg3QYQe9QaD0bcsDRQPHRJJYxKu3LtzvXIGX+U70hgPI65dLKE0t8aea+ThyDykjWWTKpB7918zZFAvyuAAiRraMYDjypoQcET0QxdiQgtmOzD/qHTu3YhOOqhWjJUkELeOwmhAgcFem8CVb8qy+vAot3qgA077tjp6ALmWVtGZxm5NwFzDrAmWYHcsizIuNTVRkLtyKgt34c8cw/vB1Ldzj0PgDnmDwLTUqIAy1a6GAKrlhwe9obMmXmuX5LUliUdMocmbBdgFHZkXw0+QEQdDnpORzJRCZIHPtyLeCFgQl633hPv/vJGypFzzPNTJb16IvgA1t+RY3E7k8B5ZCxgDmAdTgKfGa6BXafvjiGRvoe/Y73tnp/L7OLGM4TNGuzfzN6/XQohR7x0tahvZdTNigjHUdnMbGTtlmuz5GHccvrgD5YG9lvcgrS9FBwhjjIviKCkVojGImOHr6N/8J9g3Q10cPxr0WkQBSjP9kBAt0l+1WLnYSy+EEYJP3Twt8cWCWV759KpBO8E5wu1EyOUEcIrdZZuoqw8N3xPbtm+8u7dO3u+yNbSQLAhkYeQusMV7IgnDs3NaQJA6MrDt7AJv5NGrJNbvh2F9qx6rfmf4xa2nHC7uJsrz+yVw9QBO0rQKt/Cs5x3FocbsqHUMY3R59X5sAwLphTRVSPGXIgfpzSNe/lNpybfujM6qs9rmldeks68kNEEHQbT2gZCM41NhI4BiybqqHiRWbLUfrPYUuEnkrZWsyCkNaOuCoVp1G09Hwe0gfrbiHoibTxVz3t1E4CxTLgrsTMM3Nz6dAEZMSv2g49kM0oy5jzxFH8nYqdLNlvdOonijB2C+OQBj/F4SQqtHXLVUB3xmFUa0RkVMlAYXnxkCyg6KPRdE3bICoIxbLyZMr2dkjQGYWBhWVEYIyKPYPZcODF4W2o9DZK0UHu+5cXRO5RGtIVzRDaYRty8KKoRmnmcNFphnApvM1SdhTIVDW7da5+yAxuktPvZAG7Yr8szq5NT+X4+3RPyiQyWWaPMy9ZngRMxe6wGJcHjLNMRoSW8D57LdzGbanVBgJKTbSnPyxgG+7q8wSO3051yiIa7lW9A/JIqzis+RQUzYRUl8wsGRbObZmWbSdupQNbOrES5qpXNwO/ZwTgYErj8A+UhBR42iGIyFXt03gAaTiHO+YU0Yjt3az/FW/up3fralJ4XDItKt1SVW2aFW4ep29pL2VaFqpXM0qA3x91Lb9hst960aF9Sb0oyXlSd4yqqY/8aVOe4B6kOadipOsc1qw7TOYPKOj13P6WJhrvVZkD8korDzLWoMCanojJ+waA0dvMgtQkadiqOtXseqp6bKc1EqrgD8mItecfbPZH0TFbTjiYxS2FBtOsogN+0CVPgZ7K1giwnetsfUc9Xq8ObU3J29hqLvEftFExrf/qSCjgg5yuwbMgA5b3D12BhBy1+pfXJvBgazQtNTLGCJ1oajFJGfjWGXcs2u0F4+MkfioTH91Y0SOAgeVPzK/fnj6hK0jV7NHqEvo1HSs6UxzCrJqRCuMsH/rBX7bSuA2+caWYiHZTiv4TCM5m68XFNEUyJhySAgXiu9ac/jP3/K+718UykYLy2l4DTJVGKD3EQ2KnjI7TEwwcZwREWy4zv6zMguRBvFZz3XzQaHJpSlbIeY4qlp5DgmoCbn69/LTh4c6jZu1VsF3UrwysHHpaG4je4Bj4lpHFmsXM8Onq+eCGGeFKcwKlphICGc3zsCs522EkJPsKNhanM+1HUmAivQpolwu9muoCz1totiyx3vLJ9cnt61xzHePAp142RGL0l8iJvBsNJNWn24HzBWUEkNY0UpPD/NI0/YbmcP61KGlj568cZ3VgYaeAWXH0bAs4kIIoo4erJyEjVBlEqHxyOpMSpDaJ88cnI4pUTAanZH+HSgNX85gQrv40QqpbaJeEVq5OmMaAm1EtFwTWdJMEiE5WQMF5B8o/XuZlN6QpWKrBI6e0JPwCz8tbMOVhDHsJ1dszKLA/QdUoWHlRYSErpHJaBmISihsqNOMWa6gRkAgN7I9DB1DaFV2i5JuycC+rGcQJPODbLg6UhpQtyprdn9czjNTv0YBrQB95jOESczPg5fuGFD4Vy9c0Q/esoWyqgdrzyQDcwd95v60cpspHC4k8nPwri8RGCZlMzzb3fRpI/oHXqTWbQk5+W8KkBmSqSsf92YLk7Tnh3P/a5wZdy4ADxIQYjOOJi81kSJJvIyHWS/hgsFmBdhbBd/BQVlb+zpfsUbKHisubSoW9n6dmXO2qUIN/erQtVijCAheGbP3/zWjPIa7h+qI4UdFVVyrt6X6uu1HjMTLXL3NhIA3aSZcQ+6PqV1HHaLvF9s1YGi90iw7Ao3Jz9FueKCm9HpAFPK9DHhJWWO3EIYfkjDSH4mkcS8WZwwBpr6P4sq+5njbVjHf4s7ANX5s8aa+darUFvSWvSdjxNVihl+sZEX3ngXZfbaxNpkz1zNN9gwGULvkgojSqc2ph1mwToenAy7ZfWzJ2uNCo9EbtHbNkpXfmNWx4Rqd8kroiCn+Xlu8NbMZ8BPjDle/06vyG1kc7/eY62zbcg6VjHSzzOxi1Fnsbx7U6eF+YYureES7HxGLxKelmWJ1yfYLwE8tKttyix5ROZZkYQ9YasgnEMHrzNovCMTrR6R46VhPiksDYd4zvBuZSiMQiymlHIMn87vm+W1w9cC36vnsvJLnHVt5G50CiuLJJeal5EUQjWlRHLWzjjK9Rl5f2viFEHKhvAsxBXGHNv6vbN81eS0GroTN8VLVAO/Qe7hzwQZ6f4vBj7IDvSONXkyAIZuHn54vcX/wU0LHYUGEIAAA=="
+
+
+
+# ------------------------------------
+# Loader
+# ------------------------------------
+function ConvertFrom-Base64CompressedScriptBlock {
+
+    [CmdletBinding()] param(
+        [String]
+        $ScriptBlock
+    )
+
+    # Take my B64 string and do a Base64 to Byte array conversion of compressed data
+    $ScriptBlockCompressed = [System.Convert]::FromBase64String($ScriptBlock)
+
+    # Then decompress script's data
+    $InputStream = New-Object System.IO.MemoryStream(, $ScriptBlockCompressed)
+    $GzipStream = New-Object System.IO.Compression.GzipStream $InputStream, ([System.IO.Compression.CompressionMode]::Decompress)
+    $StreamReader = New-Object System.IO.StreamReader($GzipStream)
+    $ScriptBlockDecompressed = $StreamReader.ReadToEnd()
+    # And close the streams
+    $GzipStream.Close()
+    $InputStream.Close()
+
+    $ScriptBlockDecompressed
+}
+
+$AssembliesFound = $False
+$AssemblyFolder = "$PSScriptRoot\assemblies"
+if(Test-Path "$AssemblyFolder" -PathType 'Container'){
+    $Assembly = @( Get-ChildItem -Path "$AssemblyFolder\*.dll" -ErrorAction SilentlyContinue )    
+    $AssemblyCount = $Assembly.Count
+    if($AssemblyCount -gt 0 ){ $AssembliesFound = $True }
+}
+
+
+if($AssembliesFound){
+    $FoundErrors = @(
+      Foreach ($Import in @($Assembly)) {
+        try {
+            Add-Type -Path $Import.Fullname -ErrorAction Stop
+        } catch [System.Reflection.ReflectionTypeLoadException] {
+            Write-Warning "Processing $($Import.Name) Exception: $($_.Exception.Message)"
+            $LoaderExceptions = $($_.Exception.LoaderExceptions) | Sort-Object -Unique
+            foreach ($E in $LoaderExceptions) {
+                Write-Warning "Processing $($Import.Name) LoaderExceptions: $($E.Message)"
+            }
+            $true
+            #Write-Error -Message "StackTrace: $($_.Exception.StackTrace)"
+        } catch {
+            Write-Warning "Processing $($Import.Name) Exception: $($_.Exception.Message)"
+            $LoaderExceptions = $($_.Exception.LoaderExceptions) | Sort-Object -Unique
+            foreach ($E in $LoaderExceptions) {
+                Write-Warning "Processing $($Import.Name) LoaderExceptions: $($E.Message)"
+            }
+            $true
+            #Write-Error -Message "StackTrace: $($_.Exception.StackTrace)"
+        }
+      }
+    )
+
+    if ($FoundErrors.Count -gt 0) {
+        $ModuleName = (Get-ChildItem $PSScriptRoot\*.psd1).BaseName
+        Write-Warning "Importing module $ModuleName failed. Fix errors before continuing."
+        break
+    }
+}
+
+# For each scripts in the module, decompress and load it.
+# Set a flag in the Script Scope so that the scripts know we are loading a module
+# so he can have a specific logic
+$Script:LoadingState = $True
+$ScriptList = @('DoAutomaticConfig','EnvironmentVariables','General','Git','RemoveGhostDevices')
+$ScriptList | ForEach-Object {
+    $ScriptId = $_
+     $ScriptBlock = "`$ScriptBlock$($ScriptId)" | Invoke-Expression
+    $ClearScript = ConvertFrom-Base64CompressedScriptBlock -ScriptBlock $ScriptBlock
+    try{
+        $ClearScript | Invoke-Expression
+    }catch{
+        Write-Host "===============================" -f DarkGray
+        Write-Host "$ClearScript" -f DarkGray
+        Write-Host "===============================" -f DarkGray
+        Write-Error "ERROR IN script $ScriptId . Details $_"
+    }
+}
+$Script:LoadingState = $False
+
+
+
